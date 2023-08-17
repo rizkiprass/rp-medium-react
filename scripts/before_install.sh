78740637d02cbae11175e2ba08105055c823f8a8
rizkiprass <rizkiprass@gmail.com>
2023-08-17 18:08:24 +0700

    add scripts and appspec.yml2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/my-app/rp-lab-react
+cd /home/ubuntu/my-app/rp-medium-react
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
