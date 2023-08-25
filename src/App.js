59780e72eca5687c0d37c77eb7b0b4d3be10ee03
rizkiprass <rizkiprass@gmail.com>
2023-08-25 22:12:05 +0700

    delete ip at fetch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch("http://172.31.6.109:8080/api/data"); // Sesuaikan dengan URL server Express Anda
+      const response = await fetch("/api/data"); // Sesuaikan dengan URL server Express Anda
       const jsonData = await response.json();
       setData(jsonData.message);
     } catch (error) {
