c0e20f0ee2f6ad600a3a5ece84924aada94ed27a
rizkiprass <rizkiprass@gmail.com>
2023-08-26 15:59:40 +0700

    connect to db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [backendData, setBackendData] = useState({ users: [] });
+function DataDisplay() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`http://13.39.147.72:8080/api`)
+    // Fetch data from the API
+    fetch("http://13.39.147.72:8080/db")
       .then((response) => response.json())
       .then((data) => {
-        setBackendData(data);
+        setData(data);
+        setLoading(false); // Data has been fetched, so loading is done
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false in case of an error too
       });
   }, []);
 
   return (
     <div>
-      {backendData.users.length === 0 ? (
+      <h1>Data from Database</h1>
+      {loading ? (
         <p>Loading...</p>
       ) : (
-        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
 }
 
-export default App;
+export default DataDisplay;
