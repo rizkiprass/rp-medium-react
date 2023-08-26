4f6bdda9e7e5f38d327b0819ef1973b5c2697343
rizkiprass <rizkiprass@gmail.com>
2023-08-26 13:23:14 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-// function App() {
-//   const [data, setData] = useState("");
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch("http://172.31.6.109:8080/api/data"); // Sesuaikan dengan URL server Express Anda
-//       const jsonData = await response.json();
-//       setData(jsonData.message);
-//     } catch (error) {
-//       console.error("Terjadi kesalahan:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>Data dari server Express:</p>
-//         <p>{data}</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-import axios from "axios";
-
 function App() {
-  const [data, setData] = useState("");
+  const [backendData, setBackendData] = useState({ users: [] });
 
   useEffect(() => {
-    fetchData();
+    fetch(`http://13.39.147.72:8080/api`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("http://172.31.6.109:8080/api/data"); // Assuming your proxy is set up
-      setData(response.data.message);
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Data from Express server:</p>
-        <p>{data}</p>
-      </header>
+    <div>
+      {backendData.users.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
     </div>
   );
 }
