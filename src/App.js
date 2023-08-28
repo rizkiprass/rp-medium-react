a14e161dd3d55d0084fe94dbbc7f0a976b8f52d5
rizkiprass <rizkiprass@gmail.com>
2023-08-28 19:51:00 +0700

    change to /api

--- src/App.js
+++ src/App.js
@@ -1,48 +1,72 @@
 import React, { useState, useEffect } from "react";
 
-function DataDisplay() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+// function DataDisplay() {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     // Fetch data from the API
+//     fetch("http://13.39.147.72:8080/db")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setData(data);
+//         setLoading(false); // Data has been fetched, so loading is done
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//         setLoading(false); // Set loading to false in case of an error too
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Data from Database</h1>
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>Name</th>
+//               <th>Address</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {data.map((item) => (
+//               <tr key={item.id}>
+//                 <td>{item.name}</td>
+//                 <td>{item.address}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default DataDisplay;
+
+function Home() {
+  const [backendData, setBackendData] = useState({ users: [] });
 
   useEffect(() => {
-    // Fetch data from the API
-    fetch("http://13.39.147.72:8080/db")
+    fetch("/api")
       .then((response) => response.json())
       .then((data) => {
-        setData(data);
-        setLoading(false); // Data has been fetched, so loading is done
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false); // Set loading to false in case of an error too
+        setBackendData(data);
       });
   }, []);
 
   return (
     <div>
-      <h1>Data from Database</h1>
-      {loading ? (
+      {backendData.users.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.address}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
       )}
     </div>
   );
 }
 
-export default DataDisplay;
+export default Home;
