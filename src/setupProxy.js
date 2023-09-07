0616caf686c19a40a3d4ad91a499f2170369f4f4
Rizki Prasetya <rizkiprass@gmail.com>
2023-09-07 17:46:44 +0700

    [ush

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = {
-    target: 'http://localhost:8080',
+    target: "http://localhost:8080",
     changeOrigin: true
 }
 module.exports = function(app) {
