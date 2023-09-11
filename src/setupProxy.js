56ebcaddf51cfd92d9ad8a719198f988122b4e72
Rizki Prasetya <rizkiprass@gmail.com>
2023-09-11 14:25:17 +0700

    edit proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = {
-    target: "http://localhost:8080",
-    changeOrigin: true
-}
+
 module.exports = function(app) {
   app.use(
     '/api',
-    createProxyMiddleware(proxy)
+    createProxyMiddleware({
+      target: 'http://localhost:8080', // URL backend Anda
+      changeOrigin: true,
+    })
   );
 };
