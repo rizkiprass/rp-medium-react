40af7a87f503094494596f6ffd9e7c1da4e5b7ad
rizkiprass <rizkiprass@gmail.com>
2023-09-29 16:58:44 +0700

    finish add image at create

--- src/components/crud-components/CreateProduct.js
+++ src/components/crud-components/CreateProduct.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function CreateProduct({ onProductAdded }) {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null); // State untuk menyimpan file gambar yang akan diunggah
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -12,14 +13,23 @@ function CreateProduct({ onProductAdded }) {
     setPrice(e.target.value);
   };
 
+  const handleImageChange = (e) => {
+    // Mengambil file gambar dari input
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
   const handleAddProduct = () => {
+    // Membuat objek FormData untuk mengirim data yang mengandung gambar
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("price", price);
+    formData.append("image", image);
+
     // Kirim permintaan POST ke API untuk menambahkan produk baru
     fetch(`${process.env.REACT_APP_DBECOM_ENDPOINT}/api/products`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, price }),
+      body: formData,
     })
       .then((response) => response.json())
       .then((data) => {
@@ -27,6 +37,7 @@ function CreateProduct({ onProductAdded }) {
         onProductAdded(data.message);
         setName("");
         setPrice("");
+        setImage(null); // Reset input gambar setelah berhasil
       })
       .catch((error) => {
         console.error("Error adding product:", error);
@@ -47,6 +58,11 @@ function CreateProduct({ onProductAdded }) {
         placeholder="Price"
         value={price}
         onChange={handlePriceChange}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
       />
       <button onClick={handleAddProduct}>Add Product</button>
     </div>
