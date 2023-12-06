c3d3114ca5b33d6392fb94c187b286f3134e88c0
rizkiprass <rizkiprass@gmail.com>
2023-12-06 12:40:46 +0700

    fresh install

--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/my-app/rp-medium-react
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs
--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
-// import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import DataDisplay from "./components/DataDisplay";
-import ApiDisplay from "./components/ApiDisplay";
-import EcommerceDisplay from "./components/EcommerceDisplay";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Test Page</p>
-    </div>
-  );
-}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/api">API User Display</Link>
-            </li>
-            <li>
-              <Link to="/db">Data Display</Link>
-            </li>
-            <li>
-              <Link to="/ecommerce">Ecommerce Display</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/api" element={<ApiDisplay />} />
-          <Route path="/db" element={<DataDisplay />} />
-          <Route path="/ecommerce" element={<EcommerceDisplay />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
