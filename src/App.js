e985b03572daadb46c0e60bff5396aff2f659200
rizkiprass <rizkiprass@gmail.com>
2023-12-14 20:14:01 +0700

    remove auth on product get

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import LoginPage from './components/LoginPage';
 import ProductList from './components/ProductList';
+import HomePage from './components/HomePage';
 
 const App = () => {
   return (
@@ -14,8 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/products" element={<ProductList />} />
-          {/* Add other routes as needed */}
-          <Route path="/" element={<h2>Welcome to Your E-commerce App</h2>} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+// HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Welcome to Your E-commerce App</h2>
+      <img
+        src="https://picsum.photos/200" // Replace with the actual URL of your image
+        alt="E-commerce Banner"
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
+      {/* Add content for the home page as needed */}
+    </div>
+  );
+};
+
+export default HomePage;
