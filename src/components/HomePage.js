2ead746386389c50e589b8ef1830c26a87b32133
rizkiprass <rizkiprass@gmail.com>
2023-12-18 23:22:48 +0700

    cache

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 const HomePage = () => {
+  const imageUrl = 'http://localhost:8080/uploads/popcat.jpg'; // Replace with the actual image filename
   return (
     <div>
       <h2>Welcome to Your E-commerce App</h2>
@@ -9,6 +10,11 @@ const HomePage = () => {
         src="https://picsum.photos/200" // Replace with the actual URL of your image
         alt="E-commerce Banner"
         style={{ maxWidth: '100%', height: 'auto' }}
+      />
+      <img
+        src={imageUrl}
+        alt="popcatimagelocal"
+        style={{ maxWidth: '10%', height: 'auto' }}
       />
       {/* Add content for the home page as needed */}
     </div>
